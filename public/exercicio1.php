fdf72146d46d9f3e7a1386cd911b07172bc06177
Israel Cena <israelsjm@gmail.com>
2022-05-26 11:26:03 -0300

    fix exercicio1

--- public/exercicio1.php
+++ public/exercicio1.php
@@ -8,8 +8,18 @@
 if(isset($_POST['value1']) && isset($_POST['value2'])){
     $value1 = $_POST['value1'];
     $value2 = $_POST['value2'];
-    $result = $value1 + $value2;
-    echo "O resultado da soma é: $result";
+    $resultSum = $value1 + $value2;
+    $resultSub = $value1 - $value2;
+    $resultMult = $value1 * $value2;
+    $resultDiv = $value1 / $value2;
+    $resultMod = $value1 % $value2;
+    $resultExp = $value1 ** $value2;
+    echo "O resultado da soma é: $resultSum" . "<br>";
+    echo "O resultado da subtração é: $resultSub" . "<br>";
+    echo "O resultado da multiplicação é: $resultMult" . "<br>";
+    echo "O resultado da divisão é: $resultDiv" . "<br>";
+    echo "O resultado do módulo é: $resultMod" . "<br>";
+    echo "O resultado da exponenciação é: $resultExp" . "<br>";
 }
 
 echo "<br/>";
