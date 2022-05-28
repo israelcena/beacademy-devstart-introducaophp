84f0efa62ea7b95d21ff1734836a5c6b31462973
Israel Cena <israelsjm@gmail.com>
2022-05-28 20:12:09 -0300

    include exercicio2

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
-echo "<h1>Exercícios de PHP </h1>";
+echo "<h1>Exercícios de PHP</h1>";
 
 echo "<ol>";
-echo "<li><a href=\"./exercicio1.php\">Exercício 1 - Calculadora ?</a></li>";
+echo "<li><a href=\"./exercicio1.php\">Exercício 1 - Calculadora</a></li>";
+echo "<li><a href=\"./exercicio2.php\">Exercício 2 - Tabela</a></li>";
 echo "</ol>";
