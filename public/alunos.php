8d65c3cc5127d6306f4ec982a7f42755f179db88
Israel Cena <israelsjm@gmail.com>
2022-05-29 20:56:16 -0300

    include alunos

---
+++ public/alunos.php
@@ -0,0 +1,26 @@
+<?php
+
+$alunos = [
+  [
+    "nome" => "João",
+    "nota" => 7.5
+  ],
+  [
+    "nome" => "Maria",
+    "nota" => 9.5
+  ],
+  [
+    "nome" => "Pedro",
+    "notas" => [8.5, 2, 3]
+  ],
+  [
+    "nome" => "Ana",
+    "nota" => 6.5
+  ],
+  [
+    "nome" => "José",
+    "nota" => 10
+  ]
+];
+
+var_dump($alunos);
--- public/index.php
+++ public/index.php
@@ -7,4 +7,5 @@ echo "<li><a href=\"./exercicio2.php\">Exercício 2 - Tabela</a></li>";
 echo "<li><a href=\"./match.php\">Match</a></li>";
 echo "<li><a href=\"./arrays.php\">Arrays</a></li>";
 echo "<li><a href=\"./bandas.php\">Bandas</a></li>";
+echo "<li><a href=\"./alunos.php\">Alunos</a></li>";
 echo "</ol>";
