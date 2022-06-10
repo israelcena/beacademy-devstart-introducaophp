a2763ce23bee1c1ded5d426e5c479685b00ac776
israelcena <israelsjm@gmail.com>
2022-06-09 22:02:05 -0300

    change hash

--- public/argon2.php
+++ public/argon2.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 $senha = '123456';
 
-echo password_hash($senha, PASSWORD_ARGON2ID);
+echo password_hash($senha, PASSWORD_ARGON2I);
+echo "<br/>";
+echo password_hash($senha, PASSWORD_ARGON2I);
 
 echo "<br/>";
 echo "<br/>";
