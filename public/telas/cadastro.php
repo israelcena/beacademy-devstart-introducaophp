0a48810ffb7692e2dad6f752778bd3e2d9bd9c1d
israelcena <israelsjm@gmail.com>
2022-06-10 20:09:57 -0300

    remove password

--- public/telas/cadastro.php
+++ public/telas/cadastro.php
@@ -1,13 +1,7 @@
 <div class="container m-4">
-  <form action="arquivo.php" method="post">
+  <form action="" method="post">
     <label for="nome">Nome: </label>
     <input class="form-control mb-3" type="text" name="nome" id="nome" placeholder="ex: Fulano da Silva Souza" required>
-
-    <label for="senha">Senha: </label>
-    <input class="form-control mb-3" type="password" name="senha" id="senha" placeholder="ex: !@#12AsdQ" required>
-
-    <label for="resenha">Repita a Senha: </label>
-    <input class="form-control mb-3" type="password" name="resenha" id="resenha" placeholder="ex: !@#12AsdQ" required>
 
     <label for="email">E-mail</label>
     <input class="form-control mb-3" type="email" name="email" id="email" placeholder="ex: usuario@provedor.com.br" required>
